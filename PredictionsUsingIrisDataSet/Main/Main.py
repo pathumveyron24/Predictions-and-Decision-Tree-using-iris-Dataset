c8929152c038b9b31e9d84a8318e10e64607ba6b
Kalhara Perera <adorekasun@gmail.com>
2019-07-06 11:54:24 +0530

    Changed settings.json

--- PredictionsUsingIrisDataSet/Main/Main.py
+++ PredictionsUsingIrisDataSet/Main/Main.py
@@ -5,11 +5,11 @@ from sklearn.externals.six import StringIO
 import pydot
 
 iris = load_iris()
-idx = [0,50,100]
+idx = [0, 50, 100]
 
 # training data
 trainTarget = np.delete(iris.target, idx)
-trainData = np.delete(iris.data, idx,axis=0)
+trainData = np.delete(iris.data, idx, axis=0)
 
 # testing data
 testTarget = iris.target[idx]
@@ -18,16 +18,18 @@ testData = iris.data[idx]
 clf = tree.DecisionTreeClassifier()
 clf.fit(trainData, trainTarget)
 
-print (testTarget)
+print(testTarget)
 print(clf.predict(testData))
 
 # printing the decision tree
 dotData = StringIO()
 dot_data = tree.export_graphviz(clf, out_file=dotData,
-                    feature_names=iris.feature_names,
-                    class_names=iris.target_names,
-                    filled=True, rounded=True,
-                    impurity=False)
+                                feature_names=iris.feature_names,
+                                class_names=iris.target_names,
+                                filled=True, rounded=True,
+                                impurity=False)
 graph = pydot.graph_from_dot_data(dotData.getvalue())
+
+# writing the values of the tree to a visualizable tree and printing to a pdf
 graph[0].write_pdf("iris.pdf")
 print('Pdf created succesfully!')
